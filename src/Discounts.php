7838be975e4e59e12ebf48c1a3c461ae36f0b6ee
8ctopus <hello@octopuslabs.io>
2026-05-15 09:50:30 +0500

    Fix create discount expires at

--- src/Discounts.php
+++ src/Discounts.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Oct8pus\Paddle;
 
 use DateTime;
+use DateTimeInterface;
 use JsonException;
 
 class Discounts extends RestBase
@@ -95,7 +96,7 @@ class Discounts extends RestBase
             'discount_group_id' => $discountGroup,
             'restrict_to' => $restrictTo, // product or prices
             'usage_limit' => $usageLimit, // integer or null for unlimited
-            'expires_at' => $expiresAt,
+            'expires_at' => $expiresAt->format(DateTimeInterface::RFC3339),
 
             'recur' => $recurr,
             'custom_data' => null,
